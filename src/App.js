2c2b9de9ad45de6bdfdb87b277fffd7e6fd44588
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-04-18 10:24:57 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Header from './components/Pages/Header/Header';
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from './components/Pages/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Header></Header>
+
+      <Footer></Footer>
     </div>
   );
 }
